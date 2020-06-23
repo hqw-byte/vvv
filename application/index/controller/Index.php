87ae9dcb13442da6cba478f4d600b76d0f3c2fc4
Your Name <you@example.com>
2020-06-23 11:25:14 +0800

    abc

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,86 +1,157 @@
 <?php
 namespace app\index\controller;
+
 use think\Controller;
 use think\Session;
-
 class Index extends Controller
 {
     public function index()
     {
-        return $this->fetch('index');
-    }
+        // zhangsan 2020-06-18 16:48:28
+        // lisi 2020-06-17 16:48:28
+        // wangwu   2020-06-16 16:48:28
+        // hqwqwq   2020-06-15 16:48:28
+        $keyword = input('get.keyword','');
+        $startTime = input('get.startTime','');
+        $endTime = input('get.endTime','');
 
+        // var_dump($keyword);
+        $where = [];
+        if(!empty($keyword)){
+            $where = [
+                'username' => [
+                    'like','%'. $keyword.'%'
+                ]
+            ];
+        }
+        if(!empty($startTime) && !empty($endTime)){
+            $startTime = strtotime($startTime);
+            $endTime = strtotime($endTime);
+            // var_dump($startTime);
+            // var_dump($endTime);
+            $where = [
+                'loginTime' => [
+                    ['<=',$endTime],
+                    ['>=',$startTime]
+                ]
+            ];
+        }
+        $result = db('wjx_user')->where($where)->paginate(2);
+        return $this->fetch('index',['list'=>$result]);
+        
+    }
     public function login()
     {
         return $this->fetch('login');
+       
+       
     }
-    public function reg()
-    {
-        return $this->fetch('reg');
-    }
-    public function doReg(){
-        $username = input('post.username','');
-        $password = input('post.pwd','');
-        $result = db('wjx_user')->where('username',$username)->find();
-        if($result){
-            $resultJson = [
-                'error_code' => 10001,
-                'msg' => '账号重复，请重新输入!'
-            ];
-        }else{
-           
-            $where = [
-                'username'=>$username,
-                'password'=>$password,
-                'address'=>'厦门'
-            ];
-            $result = db('wjx_user')->insert($where, true);
-            if($result){
-                $resultJson = [
-                    'error_code' => 10000,
-                    'msg' => '注册成功!'
-                ];
-            }
-
-        }
-        echo json_encode($resultJson);
-    }
-    // 登录功能
     public function dologin(){
+        
         $username = input('post.username','');
         $password = input('post.pwd','');
         $code = input('post.code','');
+        // var_dump($username);
+        // var_dump($password);
+        // var_dump($code);
+        $errorCode = config('Error_code');
+
         if(!captcha_check($code)){
-            // 验证失败
+            //验证失败
             // echo '验证码错误';
             $resultJson = [
-                'error_code' => 10002,
-                'msg' => '验证码错误!'
+                'error_code' => 10001,
+                'msg' => $errorCode[10001]
             ];
-            echo json_encode($resultJson);  
         }else{
-            $where = [
-                'username'=>  $username,
-                'password'=>  $password
-            ];
-            $result = db('wjx_user')->where($where)-> find();
-            if($result){
+           
+           $where = [
+            'username'=>  $username,
+            'password'=>  $password
+           ];
+           $result = db('wjx_user')->where($where)-> find();
+           if($result){
                 $resultJson = [
                     'error_code' => 10000,
-                    'msg' => '登录成功!'
+                    'msg' => $errorCode[10000]
                 ];
-                // Session::set('name','thinkphp');
                 Session::set('username', $username);
-                echo json_encode($resultJson);  
-            }else{
-                $resultJson = [
-                    'error_code' => 10001,
-                    'msg' => '账号或密码错误，登录失败!'
-                ];
-                echo json_encode($resultJson);  
-               
-            }
-        }
 
+           }else{
+            $resultJson = [
+                'error_code' => 10002,
+                'msg' => $errorCode[10002]
+            ];
+           }
+        };
+        echo json_encode($resultJson);
+    }
+    public function addUser(){
+        $username = input('post.username','');
+        $password = input('post.pwd','');
+        $address = input('post.address','');
+        // var_dump($username);
+        // var_dump($password);
+        // var_dump($address);
+        $data = [
+            'username' => $username,
+            'password' => $password,
+            'address' => $address
+
+        ];
+        $result = db('wjx_user')->insert($data);
+        // var_dump($result);
+        if($result){
+            $resultJson = [
+                'error_code' => 10000,
+                'msg' => '添加成功'
+            ];
+        }else{
+            $resultJson = [
+                'error_code' => 10001,
+                'msg' => '添加失败'
+            ];
+        }
+        echo json_encode($resultJson);
+    }
+    public function delUser(){
+        $userid = input('post.userid','');
+        $result = db('wjx_user')->where('id',$userid)->delete();
+        if($result){
+            $resultJson = [
+                'error_code' => 10000,
+                'msg' => '删除成功'
+            ];
+        }else{
+            $resultJson = [
+                'error_code' => 10001,
+                'msg' => '删除失败'
+            ];
+        }
+        echo json_encode($resultJson);
+    }
+    public function editUser(){
+        $username = input('post.username','');
+        $password = input('post.pwd','');
+        $address = input('post.address','');
+        $id = input('post.id','');
+        $where = [
+            'username'=>$username,
+            'password'=>$password,
+            'address'=>$address,
+        ];
+        $result = db('wjx_user')->where('id',$id)->update($where);
+        if($result){
+            $resultJson = [
+                'error_code' => 10000,
+                'msg' => '修改成功'
+            ];
+        }else{
+            $resultJson = [
+                'error_code' => 10001,
+                'msg' => '修改失败'
+            ];
+        }
+        echo json_encode($resultJson);
     }
 }
